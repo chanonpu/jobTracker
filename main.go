6b21ed6621225ee3356734ba32381a87ba7b63f5
chanonpu <darknezz.pun@gmail.com>
2025-06-10 01:11:20 -0400

    add cors

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/jobTracker/config"
 	"github.com/jobTracker/models"
@@ -12,6 +13,15 @@ func main() {
 	config.DB.AutoMigrate(&models.Job{})
 
 	r := gin.Default()
+
+	//enable cors
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type"},
+		AllowCredentials: true,
+	}))
+
 	routes.JobRoutes(r)
 
 	r.Run(":8080")
